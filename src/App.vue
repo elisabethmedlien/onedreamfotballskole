71241e8a02afb05ba89cc0fb3188f89fdf42c102
Elisabeth Medlien <elisabethmedlien@gmail.com>
2021-08-08 14:10:17 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  </div> -->
+  <router-view />
 </template>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #b1b1b1;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <div class="header">
+    <img class="header-image" alt="fotball bane" src="../assets/header.jpg" />
+    <img class="logo" alt="logo" src="../assets/logo.png" />
+    <hgroup>
+      <h1>OneDream Fotballskole</h1>
+      <h2>- som verktøy for integrering</h2>
+    </hgroup>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+}
+</script>
+
+
+<style scoped>
+
+.header {
+  position: relative;
+  text-align: center;
+  color: rgba(255,255,255,0.9);
+  margin-left: -20px;
+  margin-right: -20px;
+  margin-top: -15px;
+}
+
+.header-image {
+  transform: rotate(0deg);
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+  width: 100%;
+  filter: brightness(80%);}
+
+hgroup {
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+h1 {
+  font-size: 26px;
+  font-weight: 700;
+  line-height: 24px;
+}
+
+h2 {
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 26px;
+}
+
+.logo {
+  width: 150px;
+  position: absolute;
+  top: 25px;
+  left: 35px;
+
+}
+
+</style>
+
+
